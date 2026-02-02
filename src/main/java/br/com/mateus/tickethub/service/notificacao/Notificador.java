b06a61986b0a743acd67bdf0d26ccb36b9621c93
Mateus Silva <tech.mateussilva@gmail.com>
2026-02-02 14:40:30 -0300

    refactor: Implementar arquitetura limpa e serviços de domínio essenciais - Reorganizar a estrutura do projeto em pacotes orientados a funcionalidades - Implementar o módulo de Usuário com DTOs, Serviço e Exclusão Lógica - Implementar os domínios de Evento e Localização com persistência em memória - Configurar a hierarquia base de DomainException e a infraestrutura de Notificação - Adicionar o VendaService para orquestração de compras

--- src/main/java/br/com/mateus/tickethub/service/notificacao/Notificador.java
+++
@@ -1,7 +0,0 @@
-package br.com.mateus.tickethub.service.notificacao;
-
-import br.com.mateus.tickethub.model.Usuario;
-
-public interface Notificador {
-    void enviar(Usuario destinatario, String assunto, String mensagem);
-}
